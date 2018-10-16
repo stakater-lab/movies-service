67dd1e30074c07217e674da6bb07b30dba80559c
Rasheed Amir <rasheed@aurorasolutions.io>
2018-10-16 23:06:41 +0200

    add how to generate LOGGER so, that we can easily copy to different classes

--- src/main/java/com/stakater/lab/moviesservice/MovieServiceApplication.java
+++ src/main/java/com/stakater/lab/moviesservice/MovieServiceApplication.java
@@ -1,14 +1,22 @@
 package com.stakater.lab.moviesservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+
+import java.lang.invoke.MethodHandles;
 
 @SpringBootApplication
 @EnableNeo4jRepositories(basePackages = "com.stakater.lab.moviesservice.repository")
 public class MovieServiceApplication {
 
+	// Take advantage of a Java 8 feature to create loggers like this, to prevents copy-paste errors!
+	private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
 	public static void main(String[] args) {
+		LOGGER.info("Starting application ...");
 		SpringApplication.run(MovieServiceApplication.class, args);
 	}
 
